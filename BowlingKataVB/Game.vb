d90de383a893c1d6a8d293760c9356367eddae97
skatworks <jean_paul_gaultier@msn.com>
2020-10-24 23:38:36 +0900

    ストライク、パーフェクトゲームまで対応。

--- BowlingKataVB/Game.vb
+++ BowlingKataVB/Game.vb
@@ -1,13 +1,15 @@
 ﻿Public Class Game
 
-    Private score As Integer = 0
+    Private Rolls(21) As Integer
+    Private currentRoll As Integer = 1
 
     ''' <summary>
     ''' 1投で倒したピンの数
     ''' </summary>
     ''' <param name="pins">1投で倒したピンの数</param>
     Public Sub Roll(ByVal pins As Integer)
-        score += pins
+        Rolls(currentRoll) = pins
+        currentRoll = currentRoll + 1
     End Sub
 
     ''' <summary>
@@ -15,6 +17,21 @@
     ''' </summary>
     ''' <returns>スコア</returns>
     Public Function GetScore() As Integer
+        Dim score As Integer = 0
+        Dim frameIndex As Integer = 1
+        For frame As Integer = 1 To 10
+            If (Rolls(frameIndex) = 10) Then
+                score += 10 + Rolls(frameIndex + 1) + Rolls(frameIndex + 2)
+                frameIndex = frameIndex + 1
+            ElseIf (Rolls(frameIndex) + Rolls(frameIndex + 1)) = 10 Then
+                score += 10 + Rolls(frameIndex + 2)
+                frameIndex = frameIndex + 2
+            Else
+                score += Rolls(frameIndex) + Rolls(frameIndex + 1)
+                frameIndex = frameIndex + 2
+            End If
+        Next
+
         Return score
     End Function
 End Class
--- BowlingKataVBTest/GameTest.vb
+++ BowlingKataVBTest/GameTest.vb
@@ -17,9 +17,7 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
     ''' すべてガターの場合
     ''' </summary>
     <TestMethod()> Public Sub AllGutterTest()
-        For i As Integer = 1 To 20
-            game.Roll(0)
-        Next
+        RollMany(20, 0)
         Assert.AreEqual(game.GetScore(), 0)
     End Sub
 
@@ -27,10 +25,49 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
     ''' すべて1ピンだった場合
     ''' </summary>
     <TestMethod()> Public Sub AllOneRollTest()
-        For i As Integer = 1 To 20
-            game.Roll(1)
-        Next
+        RollMany(20, 1)
         Assert.AreEqual(game.GetScore(), 20)
+    End Sub
+
+    ''' <summary>
+    ''' １回スペアを出した場合
+    ''' </summary>
+    <TestMethod()> Public Sub OneSpareTest()
+        game.Roll(5)
+        game.Roll(5)
+        game.Roll(3)
+        RollMany(17, 0)
+        Assert.AreEqual(game.GetScore(), 16)
+    End Sub
+
+    ''' <summary>
+    ''' １回ストライクを出した場合
+    ''' </summary>
+    <TestMethod()> Public Sub OneStrikeTest()
+        game.Roll(10)
+        game.Roll(3)
+        game.Roll(4)
+        RollMany(17, 0)
+        Assert.AreEqual(game.GetScore(), 24)
+    End Sub
+
+    ''' <summary>
+    ''' パーフェクトゲームだった場合
+    ''' </summary>
+    <TestMethod()> Public Sub PerfectGameTest()
+        RollMany(12, 10)
+        Assert.AreEqual(game.GetScore(), 300)
+    End Sub
+
+    ''' <summary>
+    ''' 多数回投球して、その際倒したピン数をまとめて入力
+    ''' </summary>
+    ''' <param name="n">投球回数</param>
+    ''' <param name="pins">その都度倒したピンの数</param>
+    Private Sub RollMany(ByVal n As Integer, ByVal pins As Integer)
+        For i As Integer = 1 To n
+            game.Roll(pins)
+        Next
     End Sub
 
 End Class
